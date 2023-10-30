b0d0225282a0e08d5736cdd9b17b6acf17232b34
Scott Crowder <scrowder@gmail.com>
2023-10-30 09:55:07 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by SCOTT CROWDER on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
