efdfa09ce06a5cbb43c3f8bedd60994e22ec5030
Scott Crowder <scrowder@gmail.com>
2023-10-30 17:44:12 -0500

    move tutorial files and create AddExpenseView

---
+++ iExpense/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  iExpense
+//
+//  Created by SCOTT CROWDER on 10/30/23.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
