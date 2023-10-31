047ffaf15f0ec94c57121d5d7307a15af9615f2a
Scott Crowder <scrowder@gmail.com>
2023-10-31 10:43:36 -0500

    create expense report

---
+++ iExpense/ExpenseReport.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseReport.swift
+//  iExpense
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct ExpenseReport: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseReport()
+}
