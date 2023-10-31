16030d745bfeb04d4b1e8da92749c903f32383ce
Scott Crowder <scrowder@gmail.com>
2023-10-31 11:25:53 -0500

    create user configurations

---
+++ iExpense/UserConfigurations.swift
@@ -0,0 +1,18 @@
+//
+//  UserConfigurations.swift
+//  iExpense
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct UserConfigurations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserConfigurations()
+}
