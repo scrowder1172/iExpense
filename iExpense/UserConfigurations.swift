c3434fc93cfa1b7db00d42f5aa61ff5b19ff7746
Scott Crowder <scrowder@gmail.com>
2023-10-31 11:59:22 -0500

    Update configruation screen

--- iExpense/UserConfigurations.swift
+++ iExpense/UserConfigurations.swift
@@ -10,27 +10,33 @@ import SwiftUI
 struct UserConfigurations: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var lowAmount: Double = 10
-    @State private var highAmount: Double = 100
+    @State private var lowAmount: Double = UserDefaults.standard.double(forKey: "lowAmount") == 0 ? 10 : 
+        UserDefaults.standard.double(forKey: "lowAmount")
+    @State private var highAmount: Double = UserDefaults.standard.double(forKey: "highAmount") == 0 ? 100 : UserDefaults.standard.double(forKey: "highAmount")
     
     var body: some View {
-        VStack {
-            Form {
-                Section("Low Amount") {
-                    TextField("Amount", value: $lowAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        NavigationStack {
+            VStack {
+                Form {
+                    Text("You can set the amount levels when the colors of the expenses will change.")
+                    Section("Low Amount") {
+                        TextField("Amount", value: $lowAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                    
+                    Section("High Amount") {
+                        TextField("Amount", value: $highAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
                 }
                 
-                Section("High Amount") {
-                    TextField("Amount", value: $highAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                ExpenseButton(label: "Save Configurations") {
+                    UserDefaults.standard.set(lowAmount, forKey: "lowAmount")
+                    UserDefaults.standard.set(highAmount, forKey: "highAmount")
+                    dismiss()
                 }
             }
-            
-            ExpenseButton(label: "Save Configurations") {
-                UserDefaults.standard.set(lowAmount, forKey: "lowAmount")
-                UserDefaults.standard.set(highAmount, forKey: "highAmount")
-                dismiss()
-            }
+            .navigationTitle("User Configurations")
         }
+        
     }
     
     func formatAsCurrency(_ amount: Double) -> String {
