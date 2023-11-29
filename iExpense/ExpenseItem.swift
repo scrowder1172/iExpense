702b2ed318bef7b29d645b17889fe49a81649400
Scott Crowder <scrowder@gmail.com>
2023-11-29 14:29:02 -0600

    create ExpenseItem model

--- iExpense/ExpenseItem.swift
+++ iExpense/ExpenseItem.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model final class ExpenseItem {
+    var id: UUID = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+    var dateAdded: Date = Date()
+    
+    init(id: UUID, name: String, type: String, amount: Double, dateAdded: Date) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.amount = amount
+        self.dateAdded = dateAdded
+    }
+}
+
+/*
+ struct ExpenseThing: Identifiable, Codable {
+     // struct containing expense details
+     // added Codable to allow for encoding and decoding of items
+     var id: UUID = UUID()
+     let name: String
+     let type: String
+     let amount: Double
+     var dateAdded: Date = Date()
+ }
+ 
+ */
