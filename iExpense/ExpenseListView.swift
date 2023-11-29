5a184d90b4f7e0979c100b26c40fee4079c53a9c
Scott Crowder <scrowder@gmail.com>
2023-11-29 15:40:49 -0600

    add file

---
+++ iExpense/ExpenseListView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseListView.swift
+//  iExpense
+//
+//  Created by SCOTT CROWDER on 11/29/23.
+//
+
+import SwiftUI
+
+struct ExpenseListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseListView()
+}
